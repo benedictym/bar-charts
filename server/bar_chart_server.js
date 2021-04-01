2751d0060d5bcd91275e02923e422334958f332c
Benedicty <bmambi12@icloud.com>
2021-04-01 23:56:45 +0100

    working deployement

--- server/bar_chart_server.js
+++ server/bar_chart_server.js
@@ -4,19 +4,19 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const routes = require('./routes');
-const passport = require('passport');
-const session = require('express-session');
-const flash = require("connect-flash");
+// const passport = require('passport');
+// const session = require('express-session');
+// const flash = require("connect-flash");
 
 // connect to database
-const MongoClient = require("mongodb").MongoClient;
-const mongo_pass = fs.readFileSync("public/mongo_pass.txt");
-const uri = `mongodb+srv://benedictym:${mongo_pass}@lineages.mniij.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex:true
-});
+// const MongoClient = require("mongodb").MongoClient;
+// const mongo_pass = fs.readFileSync("public/mongo_pass.txt");
+// const uri = `mongodb+srv://benedictym:${mongo_pass}@lineages.mniij.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// const client = new MongoClient(uri, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex:true
+// });
 
 app.set("port", process.env.PORT || 8080);
 
